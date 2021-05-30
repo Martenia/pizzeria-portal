988ba2f40019abe049989f0299744fa59d40321b
Marta Nitek <marta.nitek@onet.pl>
2021-05-30 18:53:47 +0200

    React Thunk i Axios

--- src/App.js
+++ src/App.js
@@ -8,28 +8,33 @@ import Kitchen from './components/views/Kitchen/Kitchen';
 import Booking from './components/views/Booking/Booking';
 import Event from './components/views/Event/Event';
 import Tables from './components/views/Tables/Tables';
-import New from './components/views/New/New';
+import New from './components/views/New/NewOrderContainer';
 import Order from './components/views/Order/Order';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
 
 const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <MainLayout>
-          <Switch>
-          <Route exact path={process.env.PUBLIC_URL + '/'} component={Homepage} />
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
-          <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component = {Event}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Waiter} />
-          <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component = {New}/>
-          <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component = {Order}/>
-          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-          </Switch>
-        </MainLayout>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <MainLayout>
+            <Switch>
+             <Route exact path={process.env.PUBLIC_URL + '/'} component={Homepage} />
+             <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+             <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+             <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
+             <Route exact path={process.env.PUBLIC_URL + '/tables/event/:id'} component = {Event}/>
+             <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Waiter} />
+             <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component = {New}/>
+             <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component = {Order}/>
+             <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+            </Switch>
+          </MainLayout>
+        </BrowserRouter>
+      </div>
+    </Provider>
+    
   );
 }
 
